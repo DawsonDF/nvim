4d3c5af0aa04910b3a6ed41448a7667c5d6741f1
DawsonDF <rudydog56@gmail.com>
2024-05-02 23:20:30 -0500

    Added keymappings, added COC

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local map = vim.keymap.set
+local set = vim.opt
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+
+-- set leader key 
+vim.g.mapleader = ","
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -15,5 +22,20 @@ require("lazy").setup({
 	{import = "plugins"}
 })
 
-
 vim.cmd.colorscheme "moonfly"
+local builtin = require('telescope.builtin')
+
+-- CUSTOM KEYBINDS
+
+-- Telescope
+map('n', '<Leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- COC (Conquer of Completion)
+vim.keymap.set("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+vim.keymap.set("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+
